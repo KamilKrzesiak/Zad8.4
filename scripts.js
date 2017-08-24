8de18a2e6bedea6133b0bbbfb6e476e6ed9f4648
kamil.krzesiak <kamil.krzesiak01@gmail.com>
2017-08-24 23:10:48 +0200

    nowe pliki

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Tu znajduje sie alert
+alert("Hello! I am an alert box!!");
+var name = prompt('Wpisz Imię;');
+// Below I have commented on a function call to disable it
+// console.log('The commented will not be called');
+alert('Hello, ' + name);
+/*
+  block comment
+  console.log('The commented will not be called');
+*/
+console.log('Hello, ' + name);
